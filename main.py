a42479b649327f7ee3de875780403fa767ac8d5a
tashpock <muratalievaziret4@gmail.com>
2022-10-13 20:46:37 +0600

    new

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ def get_html(url):
     return response.text
 
 def main():
-
-    BASE_URL = 'https://svetofor.info/sotovye-telefony-i-aksessuary/vse-smartfony/smartfony-s-podderzhkoy-4g-ru'
-    html = get_html(BASE_URL)
-    soup = get_soup(html)
-    get_data(soup)
-def get_soup(html):
-    soup = BS(html)
-    return soup
+    try:
+        for i in range(1, 1000):
+            
+            BASE_URL = f'https://svetofor.info/sotovye-telefony-i-aksessuary/vse-smartfony/smartfony-s-podderzhkoy-4g-ru/page-{i}'
+            html = get_html(BASE_URL)
+            soup = get_soup(html)
+            get_data(soup)
+            print(f'спарсили {i} страницу ')
+    except: AttributeError
+    print('Конец. Это была послеедняя страница')
 
 def get_data(soup):
     catalog = soup.find('div', class_='grid-list asdads')
@@ -24,13 +26,16 @@ def get_data(soup):
         image = phone.find('img', class_='ty-pict').get('data-ssrc')
         price = phone.find('span', class_='ty-price-update').find('span').text
         
+
         write_csv({
             'title': title,
             'image': image,
             'price': price
         })
         
-
+def get_soup(html):
+            soup = BS(html)
+            return soup
 
 
 def write_csv(data):
